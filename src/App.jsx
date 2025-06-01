29913ed49b8e7a50e96e72c7d148788021eb7a65
Wilson Carlos <contato.carlosribeiro25@gmail.com>
2025-06-01 17:14:06 -0300

    remove pagina

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomePage from './Components/Pages/HomePage';
 import ProductListingPage from './Components/Pages/ProductListingPage';
-import ProductViewPage from './Components/Pages/ProductViewPage';
 import Login from './Components/Pages/Login';
 import Cadastro from './Components/Pages/Cadastro';
 import MeusPedidos from './MeusPedidos/MeusPedidos';
@@ -18,7 +17,6 @@ export default function App() {
     <Router>
       <Routes> 
         <Route path="/"element={<HomePage/>}/>
-        <Route path="/Produtos" element={<ProductListingPage/>} />
         <Route path="/Categorias" element={<ProductViewPage/>} />
         <Route path="/Login" element={<Login/>}/>
         <Route path='/Cadastro' element={<Cadastro/>}/>
