bc9cf50bf2eab45daef2314639e8bbb95d4517ab
Wilson Carlos <contato.carlosribeiro25@gmail.com>
2025-06-02 00:57:50 -0300

    remove add page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import HomePage from './Components/Pages/HomePage';
 import ProductListingPage from './Components/Pages/ProductListingPage';
 import Login from './Components/Pages/Login';
 import Cadastro from './Components/Pages/Cadastro';
-import Carrinho from './Components/Carrinho';
 import MeusPedidos from './Components/MeusPedidos/MeusPedidos';
 import Carrinho from './Components/Pages/Carrinho';
 
